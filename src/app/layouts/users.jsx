7220e0343fd75d6c8a513e322839b6006afbe000
Eleonora Prokushenkova <eleonoraprokushenkova@MacBook-Air-Eleonora.local>
2022-05-17 10:20:49 +0300

    form

---
+++ src/app/layouts/users.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import UserPage from "../components/page/userPage";
+import UsersListPage from "../components/page/usersListPage";
+import EditUserPage from "../components/page/editUserPage/editUserPage";
+
+const Users = () => {
+    const params = useParams();
+    const { userId, edit } = params;
+
+    return (
+        <>
+            {userId ? (
+                edit ? (
+                    <EditUserPage userId={userId} />
+                ) : (
+                    <UserPage userId={userId} />
+                )
+            ) : (
+                <UsersListPage />
+            )}
+        </>
+    );
+};
+
+export default Users;
